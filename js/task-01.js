a55b5aac7bc48be80881ec861e24c8bc739a230e
AlinaTantsura <133903876+AlinaTantsura@users.noreply.github.com>
2023-09-19 20:26:34 +0300

    Update

--- js/task-01.js
+++ js/task-01.js
@@ -1,6 +1,6 @@
 const list = document.querySelector("#categories");
 const categories = [...list.children];
-console.dir(`Number of categories: ${categories.length}`);
+console.log(`Number of categories: ${categories.length}`);
 categories.forEach(item => {
     console.log(`Category: ${item.querySelector('h2').textContent}`);
     console.log(`Elements: ${[...item.querySelectorAll('li')].length}`);
